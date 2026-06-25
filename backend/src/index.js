7305d6024a71e69a6d1953e95ac097cbe2855d92
Harshita Saini <harshitasaini283@gmail.com>
2026-06-25 17:18:30 +0530

    fix express wildcard route and frontend path

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,40 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 import path from "path";
 
 dotenv.config();
 
-
-const PORT= process.env.PORT;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: "http://localhost:5173",
+    credentials: true
 }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    // ✅ Fix 2: sahi path
+    app.use(express.static(path.join(__dirname, "../frontend/ChatApp/dist")));
 
-    app.get("*", (req,res) =>{
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
-    })
+    // ✅ Fix 1: new Express syntax
+    app.get("*splat", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/ChatApp/dist/index.html"));
+    });
 }
 
-server.listen(PORT, () =>{
-    console.log("server is running on PORT" + PORT)
-    connectDB()
+server.listen(PORT, () => {
+    console.log("server is running on PORT " + PORT);
+    connectDB();
 });
